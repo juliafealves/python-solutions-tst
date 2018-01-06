272099180744e7579752681a4be2d5f0d3789bc7
juliafealves <juliafealves>
2018-01-05 21:52:11 -0300

    Developed activity "Name Thrice" in Python.

---
+++ unidade1/name-thrice/name-thrice.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+# Activity: Name Thrice
+# Author: Júlia Fernandes Alves <juliafealves@gmail.com>
+
+# Description:
+# Escreva um programa que leia seu nome da entrada e o imprima 3 vezes.
+#
+# Example:
+# Dalton
+# Dalton
+# Dalton
+# Dalton
+
+name = raw_input()
+
+print name
+print name
+print name
