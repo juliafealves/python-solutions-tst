d99e743996e1ca0a121418503e773ec71cfa739f
juliafealves <juliafealves>
2018-01-06 11:24:40 -0300

    Developed activity "Corrects Average" in Python.

---
+++ unidade1/corrects-average/corrects-average.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# Activity: Corrects Average
+# Author: Júlia Fernandes Alves <juliafealves@gmail.com>
+
+# Description:
+# Escreva um programa que calcule a média de um aluno.
+
+grade1 = 10.0
+grade2 = 5.0
+
+average = (grade1 + grade2) / 2
+
+print "Nota 1: %4.1f" % grade1
+print "Nota 2: %4.1f" % grade2
+print "Média : %4.1f" % average
